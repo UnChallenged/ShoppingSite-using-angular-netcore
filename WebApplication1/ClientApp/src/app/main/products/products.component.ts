5a2f726daa41eb9d8b1d05341f78e1143c1142f9
Khizar Murad <123hashinclude@gmail.com>
2021-04-10 21:49:15 +0500

    fixed styling

--- WebApplication1/ClientApp/src/app/main/products/products.component.ts
+++ WebApplication1/ClientApp/src/app/main/products/products.component.ts
@@ -13,7 +13,7 @@ import { Cartproduct } from '../../model-ts/cartproduct';
 
 export class ProductsComponent implements OnInit {
   breakpoint: number
-  Searchvalue = 'Search';
+  Searchvalue = '';
 
   constructor(public http: HttpClient, private productservice: MainService, private cart:CartService) {
     this.getproducts();
